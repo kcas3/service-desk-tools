cd1483fb4c152cd16e7e526f6377fc0cde3cb635
Kyle <kc4s3y@gmail.com>
2025-07-01 12:07:17 -0400

    smaller list, can just set all to lower, not sure how I didn't fix that three years ago

--- phonetics.py
+++ phonetics.py
@@ -1,6 +1,6 @@
 # An easy way to have phonetics at work for spottier/bad audio calls
 
-word = input("Please input a word to be translated (all lowercase): ")
+word = input("Please input a word to be translated (all lowercase): ").lower()
 word_to_list = list(word)
 
 alphabet = { "a":"Alfa",     "b":"Bravo",    "c":"Charlie",
@@ -11,17 +11,8 @@ alphabet = { "a":"Alfa",     "b":"Bravo",    "c":"Charlie",
              "p":"Papa",     "q":"Quebec",   "r":"Romeo",
              "s":"Sierra",   "t":"Tango",    "u":"Uniform",
              "v":"Victor",   "w":"Whiskey",  "x":"X-ray",
-             "y":"Yankee",   "z":"Zulu",     "A":"Alfa", 
-             "B":"Bravo",    "C":"Charlie",  "D":"Delta", 
-             "E":"Echo",     "F":"Foxtrot",  "G":"Golf", 
-             "H":"Hotel",    "I":"India",    "J":"Juliett", 
-             "K":"Kilo",     "L":"Lima",     "M":"Mike", 
-             "N":"November", "O":"Oscar",    "P":"Papa", 
-             "Q":"Quebec",   "R":"Romeo",    "S":"Sierra", 
-             "T":"Tango",    "U":"Uniform",  "V":"Victor", 
-             "W":"Whiskey",  "X":"X-ray",    "Y":"Yankee", 
-             "Z":"Zulu",     " ":" ",        "-":"-"
-             ".":"." }
+             "y":"Yankee",   "z":"Zulu",     " ":" ",        
+             "-":"-",        ".":"." }
 
 for i in word_to_list:
     print(alphabet[i])
